177058732d573e2797f58a4527f741796a251608
Jowelss <devejowels.gitl.loper@gmail.com>
2024-02-15 16:45:24 -0400

    Hice cambios minimos solo para probar los commits

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
-const message = document.getElementById('message');
-
-function coso() {
-  // message.textContent = '';
-}
-coso();
-const array = [];
-let num = 1;
-while (num <= 50) {
-  array.push(num);
-  num++;
-}
-console.log(array);
+const button = document.getElementById('effect-button');
+button.addEventListener('click', () => {
+  console.log('Funciona correctamente');
+});
