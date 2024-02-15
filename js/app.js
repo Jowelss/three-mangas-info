81d778e367e90de605f35ae6b34fe0ce7c2ad6c6
Jowelss <devejowels.gitl.loper@gmail.com>
2024-02-15 16:04:48 -0400

    My first proyect

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const message = document.getElementById('message');
+
+function coso() {
+  // message.textContent = '';
+}
+coso();
+const array = [];
+let num = 1;
+while (num <= 50) {
+  array.push(num);
+  num++;
+}
+console.log(array);
