c0a081036d63dd788053f0ce2b368f2aff93b299
Jowelss <devejowels.gitl.loper@gmail.com>
2024-02-20 21:08:52 -0400

    Ya implemente el efecto en todos los elementos pero tengo pequeños errores, los estilos aun no los apliqué, lo hare el miercoles o el jueves

--- js/app.js
+++ js/app.js
@@ -1,22 +1,53 @@
-const button = document.getElementById('link');
-const berserk = document.getElementById('berserk');
-const coso = document.querySelectorAll('.coso');
+const infoBerserk = document.getElementById('info-berserk');
+const moveBerserk = document.querySelector('.move-berserk');
+const infoVagabond = document.getElementById('info-vagabond');
+const moveVagabond = document.querySelector('.move-vagabond');
+const infoVinlandSaga = document.getElementById('info-vinlandsaga');
+const moveVinlandSaga = document.querySelector('.move-vinlandsaga');
+const subtitle = document.querySelectorAll('.list-item__subTitle');
 let activador = true;
+let coso = true;
+let pero = true;
 
-button.addEventListener('click', () => {
-  if (activador) {
-    berserk.style.display = 'block';
+subtitle.forEach((element) => {
+  element.addEventListener('click', () => {
+    if (element.id === 'berserk' && activador) {
+      infoBerserk.style.display = 'block';
+      moveVagabond.style.top = '60px';
+      moveVinlandSaga.style.top = '60px';
 
-    coso.forEach((element) => {
-      return (element.style.top = '60px');
-    });
-    activador = false;
-  } else {
-    berserk.style.display = 'none';
+      activador = false;
+      return;
+    } else {
+      infoBerserk.style.display = 'none';
+      moveVagabond.style.top = '0px';
+      moveVinlandSaga.style.top = '0px';
 
-    coso.forEach((element) => {
-      return (element.style.top = '0px');
-    });
-    activador = true;
-  }
+      activador = true;
+    }
+
+    if (element.id === 'vagabond' && coso) {
+      infoVagabond.style.display = 'block';
+      moveVinlandSaga.style.top = '60px';
+
+      coso = false;
+      return;
+    } else {
+      infoVagabond.style.display = 'none';
+      moveVinlandSaga.style.top = '0px';
+
+      coso = true;
+    }
+
+    if (element.id === 'vinland-saga' && pero) {
+      infoVinlandSaga.style.display = 'block';
+
+      pero = false;
+      return;
+    } else {
+      infoVinlandSaga.style.display = 'none';
+
+      pero = true;
+    }
+  });
 });
